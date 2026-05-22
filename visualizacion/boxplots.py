7c1c8843c1cf1ad1a95324e349ca3bb66aa993d0
eguzki-dm <eguzkia.lab@gmail.com>
2026-05-22 13:36:08 +0200

    Graficas en lila ♥

--- visualizacion/boxplots.py
+++ visualizacion/boxplots.py
@@ -1,58 +1,23 @@
 import math
-import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def visualizar_balance_clases(y):
-    '''
-    Distribución entre clases TARGET
-    '''
-    conteo = pd.Series(y).value_counts().sort_index()
-
-    plt.figure(figsize=(8,5))
-
-    sns.barplot(
-        x=conteo.index,
-        y=conteo.values,
-        color='#C8A2C8'   # Lila suave
-    )
-
-    plt.title("Distribución de clases")
-
-    plt.xlabel("Clase")
-    plt.ylabel("Frecuencia")
-
-    plt.tight_layout()
-    plt.show()
-
-    return conteo
-
-
-
-def visualizar_distribuciones(
+def visualizar_boxplots(
     df,
-    bins=30,
-    kde=True, # False para datasets grandes
     figsize=(16, 12),
     cols_per_row=3
 ):
     """
-    Visualiza distribuciones de variables numéricas.
+    Visualiza boxplots de variables numéricas.
 
     Parameters
     ----------
     df : DataFrame
         Dataset de entrada.
 
-    bins : int
-        Número de bins histograma.
-
-    kde : bool
-        Mostrar KDE.
-
     figsize : tuple
-        Tamaño figura global.
+        Tamaño figura.
 
     cols_per_row : int
         Número columnas subplot.
@@ -79,36 +44,43 @@ def visualizar_distribuciones(
     # =====================================================
 
     fig, axes = plt.subplots(
+
         n_rows,
+
         n_cols,
+
         figsize=figsize
     )
 
-    # Convertir axes a array plano
     axes = axes.flatten()
 
     # =====================================================
-    # Histogramas
+    # Boxplots
     # =====================================================
 
     for i, col in enumerate(columns):
 
-        sns.histplot(
+        sns.boxplot(
 
-            numeric_df[col],
-
-            bins=bins,
-
-            kde=kde,
+            x=numeric_df[col],
 
             ax=axes[i],
 
             color='#C8A2C8'   # Lila suave
         )
 
-        axes[i].set_title(col)
+        axes[i].set_title(
+
+            col,
+
+            fontsize=11
+        )
 
         axes[i].set_xlabel('')
+
+        axes[i].grid(
+            alpha=0.3
+        )
 
     # =====================================================
     # Eliminar subplots vacíos
@@ -123,7 +95,7 @@ def visualizar_distribuciones(
     # =====================================================
 
     plt.suptitle(
-        'Distribución Variables Numéricas',
+        'Boxplots Variables Numéricas',
         fontsize=16
     )
 
