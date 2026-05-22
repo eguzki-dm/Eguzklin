7479cf5fcda7d975d3549bad70735424e8ef92a0
eguzki-dm <eguzkia.lab@gmail.com>
2026-05-22 13:22:24 +0200

    Añadir información de configuración en setup.py para la descripción del paquete

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 from setuptools import setup, find_packages
 
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="eguzklean",
     version="0.1.0",
     description="Funciones auxiliares para EDA, visualización y preprocesamiento",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="eguzki-dm",
+    author_email="eguzkia.lab@gmail.com",
+    url="https://github.com/eguzki-dm/Eguzklean",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     packages=find_packages(where="."),
     package_dir={"": "."},
     python_requires=">=3.8",
